ca48ccb80e28bcae141ae327cd2dd0ecef75bda9
Nanofication <nansquared@gmail.com>
2017-06-02 13:29:24 -0400

    Copied over training data for testing

---
+++ MultinomialBayesTutorial.py
@@ -0,0 +1,30 @@
+"""
+
+Following NLTK tutorial on classifying sentences and intents
+
+"""
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+#Word Stemmer. Reduce words to the root forms. Better for classifying
+stemmer = LancasterStemmer()
+
+# 3 classes of training data. Play around with this
+training_data = []
+training_data.append({"class":"greeting", "sentence":"how are you?"})
+training_data.append({"class":"greeting", "sentence":"how is your day?"})
+training_data.append({"class":"greeting", "sentence":"good day"})
+training_data.append({"class":"greeting", "sentence":"how is it going today?"})
+
+training_data.append({"class":"goodbye", "sentence":"have a nice day"})
+training_data.append({"class":"goodbye", "sentence":"see you later"})
+training_data.append({"class":"goodbye", "sentence":"have a nice day"})
+training_data.append({"class":"goodbye", "sentence":"talk to you soon"})
+
+training_data.append({"class":"sandwich", "sentence":"make me a sandwich"})
+training_data.append({"class":"sandwich", "sentence":"can you make a sandwich?"})
+training_data.append({"class":"sandwich", "sentence":"having a sandwich today?"})
+training_data.append({"class":"sandwich", "sentence":"what's for lunch?"})
+
+
